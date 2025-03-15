6c1f86babc21763a79e25e658ee26b536247e7c1
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2025-03-15 12:27:49 +0100

    Add Marketing Header and Mobile version

--- client/src/components/layout/HeaderMarketing.tsx
+++ client/src/components/layout/HeaderMarketing.tsx
@@ -1,12 +1,69 @@
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
 export default function HeaderMarketing() {
   return (
     <>
-      <header>
-        <nav className="flex bg-white h-[80px] px-10">
-          <div className="flex justify-between">
-            <div>Logo</div>
-            <div>Links</div>
-            <div>Buttons</div>
+      <header className="bg-white h-[80px] px-10">
+        <nav className="flex justify-between items-center h-full">
+          <div className="flex gap-5">
+            <div className="flex items-center">
+              <Link to="/">
+                <p>How May I Help You?</p>
+              </Link>
+            </div>
+            <div className="hidden md:flex">
+              <div>
+                <Link to="/">
+                  <Button variant="link">Home</Button>
+                </Link>
+              </div>
+              <div>
+                <Link to="/">
+                  <Button variant="link">Pricing</Button>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div className="hidden md:flex gap-3">
+              <div>
+                <Link to="/register">
+                  <Button variant="secondary">Register</Button>
+                </Link>
+              </div>
+              <div>
+                <Link to="/login">
+                  <Button>Login</Button>
+                </Link>
+              </div>
+            </div>
+            <div className="md:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <Button variant="secondary">Menu</Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem>Home</DropdownMenuItem>
+                  <DropdownMenuItem>Pricing</DropdownMenuItem>
+                  <Link to="/register">
+                    <DropdownMenuItem>Register</DropdownMenuItem>
+                  </Link>
+                  <Link to="/login">
+                    <DropdownMenuItem>Login</DropdownMenuItem>
+                  </Link>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </nav>
       </header>
